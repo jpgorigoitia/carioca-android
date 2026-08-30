ab12e22f97b84069e20e8ec21795706dda14ba73
jpgorigoitia <jpgorigoitia@gmail.com>
2026-08-30 12:46:45 -0400

    Bump playable table build to 0.5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.carioca.game"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.4.0"
+        versionCode = 5
+        versionName = "0.5.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
